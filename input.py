5227f367bc6d7db02b5cf6840e38903751cb6cd2
etienne87 <et.perot@gmail.com>
2020-01-26 16:26:26 +0100

    bugfix

--- input.py
+++ input.py
@@ -5,10 +5,10 @@ def read_file(filename):
         max_slices, _ = f.readline().split(' ')
         slices = f.readline().split(' ')  
         slices = [int(item) for item in slices]              
-        return max_slices, slices
+        return int(max_slices), slices
 
 if __name__ == '__main__':
-    max_slices, slices = read_file('input/c_medium.in')
-
+    max_slices, slices = read_file('input/e_also_big.in')
+    import pdb;pdb.set_trace()
     print(max_slices)
     print(slices)
