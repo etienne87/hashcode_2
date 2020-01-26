c2127d6572826b63f0c7dc95b72de7978318a450
raphael <raphael.roseandrieux@gmail.com>
2020-01-26 18:17:29 +0100

    raph

--- solution_raph.py
+++ solution_raph.py
@@ -34,7 +34,7 @@ def sol_online(item_weights, max_weights):
     print(max_weights)
     n = len(item_weights)
     W = max_weights
-    K = [[0 for w in range(W + 1)] for i in range(n)]
+    K = [[[0,[]] for w in range(W + 1)] for i in range(n)]
 
     # Recurrence
     for i in range(1, n):
@@ -43,12 +43,17 @@ def sol_online(item_weights, max_weights):
         vi = item_values[i]
 
         if wi <= w:
-          K[i][w] = max([K[i - 1][w - wi] + vi, K[i - 1][w]])
+          if K[i - 1][w - wi][0] + vi > K[i - 1][w][0]:
+              K[i][w][0] = K[i - 1][w - wi][0] + vi
+              K[i][w][1] = K[i - 1][w - wi][1] + [i-1]
+          else:
+              K[i][w] =  K[i - 1][w]
         else:
           K[i][w] = K[i - 1][w]
 
     # Results
-    print("Result: ", K[n - 1][W])
+    print("Result: ", K[n - 1][W][0])
+    print(K[n - 1][W])
     return K
 
     ## Optional: Uncomment to view the 2D table
@@ -64,13 +69,43 @@ def sol_online(item_weights, max_weights):
 def main():
     max_slices, slices = read_file('input/d_quite_big.in')
     print("slices", slices)
-    K_max_mem = 1000
-    id_max = 0
-    while slices[id_max] < K_max_mem:
-        id_max += 1
-        if id_max>= len(slices):
+    K_max_mem = 10000
+    id_max_mem = 0
+    while slices[id_max_mem] < K_max_mem:
+        id_max_mem += 1
+        if id_max_mem >= len(slices):
             break
-    K = sol_online(slices, max_slices)
+    K = sol_online(slices[:id_max_mem], K_max_mem)
+    big_slices = slices[id_max_mem:]
+    len_big = len(big_slices)
+    available = np.ones(len_big)
+
+    current_size = 0
+    list_id_pizza = []
+
+    while current_size < max_slices-K_max_mem:
+        if np.alltrue(available==0):
+            break
+        possib = np.where(available==1)[0]
+        id_id = np.random.randint(0, len(possib))
+        id_next = possib[id_id]
+        available[id_next] = 0
+        if big_slices[id_next] > max_slices - current_size:
+            pass
+        else:
+            current_size += big_slices[id_next]
+            list_id_pizza += [id_next + id_max_mem]
+
+
+    print("hellloooo", max_slices-current_size)
+
+#    K = sol_online(slices[:id_max_mem], max_slices - K_max_mem)
+
+
+
+
+
+
     print("max result", max_slices)
 
 
