c6adddde68d12a93bd5ebb113778f8a7b9216a28
etienne87 <et.perot@gmail.com>
2020-01-26 16:34:38 +0100

    bugfix

--- etienne.py
+++ etienne.py
@@ -4,17 +4,15 @@ from input import read_file
 def optimize_max_slices(slices, max_possible):
     total = 0
     ans = []
-    for v in slices[::-1]:
+    for i, v in enumerate(slices[::-1]):
         if (total + v) < max_possible:
-            ans.append(v)
+            ans.append(i)
             total += v
         
-        if total >= max_possible:
-            break
     return ans, total
 
 if __name__ == '__main__':
     max_slices, slices = read_file('input/e_also_big.in')
 
     ans, total = optimize_max_slices(slices, max_slices)
-    print(total, max_slices)
+    print(total, max_slices, max_slices-total)
