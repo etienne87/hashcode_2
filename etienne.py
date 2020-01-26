a7196878b1f3557c850b919a19468078e019c414
etienne87 <et.perot@gmail.com>
2020-01-26 16:39:50 +0100

    example of usage of max

--- etienne.py
+++ etienne.py
@@ -1,5 +1,5 @@
 from input import read_file
-
+from compute_score import compute_score
 
 def optimize_max_slices(slices, max_possible):
     total = 0
@@ -13,6 +13,12 @@ def optimize_max_slices(slices, max_possible):
 
 if __name__ == '__main__':
     max_slices, slices = read_file('input/e_also_big.in')
+    total_pizza_types = len(slices)
 
     ans, total = optimize_max_slices(slices, max_slices)
     print(total, max_slices, max_slices-total)
+
+
+    score = compute_score(max_slices, total_pizza_types, slices, len(ans), ans)
+
+    print(score)
