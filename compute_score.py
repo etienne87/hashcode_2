e590b8edef56490624d0b2f907acd6a9f8c32a20
Maximilian Jaritz <maxjaritz@gmail.com>
2020-01-26 16:35:15 +0100

    add score computation

---
+++ compute_score.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def compute_score(max_slices, types_of_pizza, slices_per_pizza, num_ordered_pizzas, ordered_pizzas_idx):
+    if num_ordered_pizzas > types_of_pizza:
+        raise ValueError('You ordered more pizzas that exist')
+
+    ordered_slices = sum(np.array(slices_per_pizza)[np.array(ordered_pizzas_idx)])
+
+    if ordered_slices > max_slices:
+        raise ValueError('You ordered more than the maximum number of slices')
+
+    print('Score: {}'.format(ordered_slices))
+
+
+if __name__ == '__main__':
+    compute_score(17, 4, [2, 5, 6, 8], 3, [0, 2, 3])
