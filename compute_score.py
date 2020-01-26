fb7109941919d337469d22f96c57e4f9ff93857d
etienne87 <et.perot@gmail.com>
2020-01-26 16:44:48 +0100

    small change

--- compute_score.py
+++ compute_score.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def compute_score(max_slices, types_of_pizza, slices_per_pizza, num_ordered_pizzas, ordered_pizzas_idx):
+def compute_score(max_slices, types_of_pizza, slices_per_pizza, num_ordered_pizzas, ordered_pizzas_idx, verbose=False):
     if num_ordered_pizzas > types_of_pizza:
         raise ValueError('You ordered more pizzas that exist')
 
@@ -10,7 +10,10 @@ def compute_score(max_slices, types_of_pizza, slices_per_pizza, num_ordered_pizz
     if ordered_slices > max_slices:
         raise ValueError('You ordered more than the maximum number of slices')
 
-    print('Score: {}'.format(ordered_slices))
+    if verbose:
+        print('Score: {}'.format(ordered_slices))
+    else:
+        return ordered_slices
 
 
 if __name__ == '__main__':
--- etienne.py
+++ etienne.py
@@ -1,3 +1,4 @@
+import os, glob
 from input import read_file
 from compute_score import compute_score
 
@@ -11,14 +12,28 @@ def optimize_max_slices(slices, max_possible):
         
     return ans, total
 
+def run_all(dir):
+    total = 0
+    filenames = glob.glob(dir + "/*.in")
+    print(filenames)
+    for filename in filenames:
+        max_slices, slices = read_file(filename)
+        total_pizza_types = len(slices)
+        ans, total = optimize_max_slices(slices, max_slices)
+        print("problem: ", os.path.basename(filename), ": ", total, max_slices, max_slices-total)
+        score = compute_score(max_slices, total_pizza_types, slices, len(ans), ans)
+        total += score
+    return total
+
 if __name__ == '__main__':
-    max_slices, slices = read_file('input/e_also_big.in')
-    total_pizza_types = len(slices)
 
-    ans, total = optimize_max_slices(slices, max_slices)
-    print(total, max_slices, max_slices-total)
+    # max_slices, slices = read_file('input/d_also_big.in')
+    # total_pizza_types = len(slices)
+    # ans, total = optimize_max_slices(slices, max_slices)
+    # print(total, max_slices, max_slices-total)
+    # score = compute_score(max_slices, total_pizza_types, slices, len(ans), ans)
+    # print(score)
 
+    score = run_all('input')
 
-    score = compute_score(max_slices, total_pizza_types, slices, len(ans), ans)
-
-    print(score)
+    print('total score: ', score)
