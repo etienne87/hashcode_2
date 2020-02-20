550599411aa0d609ce5d9ac48fbf61c3c65d90cf
raphael <raphael.roseandrieux@gmail.com>
2020-02-20 18:37:30 +0100

    first commit 2020 !!

---
+++ solution_raph.py
@@ -0,0 +1,46 @@
+import numpy as np
+import os
+import time
+
+
+
+
+
+
+
+
+
+def main():
+    print("test")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
