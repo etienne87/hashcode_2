f8e5fce1c21383243b567335b2ba90b77a3ff5b7
Etienne <eperot@chronocam.com>
2020-02-20 20:19:12 +0100

    test

--- etienne.py
+++ etienne.py
@@ -13,7 +13,8 @@ class Library:
         self.num_books = N
         self.sign_up_time = T
         self.shippable_per_day = M
-        self.book_ids = set(books)
+        self.books = sorted(books, key=lambda book:book.score, reverse=True)
+
 
 def my_read(file):
     num_days, book_scores, libs = read_file(file)
@@ -25,36 +26,85 @@ def my_read(file):
         sign_up_t = lib['sign_up_t']
         ship_per_day = lib['ship_per_day']
         book_ids = lib['book_ids']
-        lib_books = books[book_ids]
-        #convert to books
+        lib_books = [books[id] for id in book_ids]
         libraries.append(Library(num_books_in_lib, sign_up_t, ship_per_day, lib_books))
 
     return libraries, books, num_days
 
 
 def compute_score(library, unscanned_books, remaining_days):
-    if library.sign_up_time > remaining_days:
+    remaining_days = remaining_days - library.sign_up_time
+    if remaining_days <= 0:
         return 0
-    books_shippable = unscanned_books.difference(unscanned_books)
+
+    #books_shippable = library.books.intersection(unscanned_books)
     num_shippable = remaining_days * library.shippable_per_day
 
-    #sort books
-    books_shippable = list(books_shippable)
-    best_books = sorted(books_shippable, key=lambda book:book.score)[:num_shippable]
+    #select books
+    books_shippable = []
+    for book in library.books:
+        if unscanned_books[book.id]:
+            books_shippable.append(book)
 
-    total_score = [item.score for item in best_books]
-    return total_score, best_books
+        if len(books_shippable) >= num_shippable:
+            break
 
+    total_score = sum([item.score for item in books_shippable])
+    return total_score, books_shippable
+
+def print_scores(scores):
+    print([item[1][0] for item in scores])
 
 def optimize(libraries, all_books, days):
+
+    id_libs = []
+    n_books = []
+    books_per_lib = []
+
+    signed_up_libs = []
+    remaining_libs = [i for i in range(len(libraries))]
+    unscanned_books = [1 for i in range(len(all_books))]
+
+    total_score = 0
+
     d = 0
     while d < days:
-        pass
+        remaining_days = days-d
+
+        scores = [(i,compute_score(lib, unscanned_books, remaining_days)) for i, lib in enumerate(libraries) if remaining_libs[i]]
+
+        sorted_scores = sorted(scores, key=lambda lib:lib[1][0], reverse=True)
+
+        best_lib_id, lib_score, lib_books = sorted_scores[0]
+
+        total_score += lib_score
+
+        signed_up_libs.append(best_lib_id)
+        n_books.append(len(lib_books))
+        books_per_lib.append(lib_books)
+
+        #remove lib
+        remaining_libs[best_lib_id] = 0
+        #remove book
+        for book in lib_books:
+            unscanned_books[book.id] = 0
+
+    return total_score, id_libs, n_books, books_per_lib
 
 
+def write_solution(result_file, id_libs, n_books, books_per_lib):
+    with open(result_file, 'w') as file:
+        file.write(str(len(id_libs)) + "\n")
+        for i, indice_lib in enumerate(id_libs):
+            nbooks = n_books[i]
+            file.write(str(indice_lib) + " " + str(nbooks) + "\n")
 
+            for book_indice in books_per_lib[i]:
+                file.write(str(book_indice) + " ")
+            file.write("\n")
 
 
 if __name__ == '__main__':
 
-    libraries, books = my_read('input/a_example.txt')
+    libraries, books, num_days = my_read('input/a_example.txt')
+    optimize(libraries, books, num_days)
