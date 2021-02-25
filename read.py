905c07d3e8311f09aebd63b556d7c6b50c0fd6cc
marintoro <marin.toromanoff@gmail.com>
2021-02-25 21:49:34 +0100

    add write_file_normer

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 
-
+from functools import *
+from math import *
 
 
 def read_file(filename):
@@ -43,9 +44,28 @@ def write_file(filename, intersection_dict):
         for key, value in intersection_dict.items():
             the_file.write("{0} \n".format(key)) # intersection ID
             the_file.write("{0} \n".format(len(value))) # nbr street for schedule in intersection
-            for value_car in value:
+            for value_car in sorted(value, key = lambda i: i[1]):
                 the_file.write("{0} {1}\n".format(value_car[0],value_car[1])) # for each street in the intersection : street_name time
 
+def write_file_normer(filename, intersection_dict,nombre_magic):
+    """
+    # intersection_dict
+    {
+        3: [(rue-paris, 3), (rue-rome, 10)],
+        12: [(rue-paris, 3), (rue-rome2, 10)]
+    }
+    """
+    with open("output/{0}".format(filename), 'a') as the_file:
+        the_file.truncate(0)
+        the_file.write("{0} \n".format(len(intersection_dict))) # nbr intersection_dict
+        for key, value in intersection_dict.items():
+            the_file.write("{0} \n".format(key)) # intersection ID
+            the_file.write("{0} \n".format(len(value))) # nbr street for schedule in intersection
+            nbr_intersection = len(value)
+            total_time_schedule = reduce(lambda x,y:x+int(y[1]), value,0)
+            max_time_schedule = floor(nbr_intersection * nombre_magic)
+            for value_car in sorted(value, key = lambda i: i[1]):
+                the_file.write("{0} {1}\n".format(value_car[0], ceil(value_car[1] * max_time_schedule / total_time_schedule))) # for each street in the intersection : street_name time
 
 if __name__ == '__main__':
     streets, cars, duration, nb_inters, nb_streets, nb_cars, bonus = read_file('input/a.txt')
--- solution_thomas.py
+++ solution_thomas.py
@@ -1,10 +1,36 @@
 import numpy as np
 import time
-from read import read_file, write_file
+from read import read_file, write_file, write_file_normer
 import os
 
 
+def compute_length_path_car(list_cars, streets):
+    tab_time_cars = []
+    for car in list_cars:
+        total_time = 0
+        for street_name in car[1:]:
+            time_travel_street = streets[street_name][2]
+            total_time += time_travel_street
+        tab_time_cars.append(total_time)
+    return np.array(tab_time_cars)
 
+def compute_length_path_car_with_solution(list_cars, streets, dict_schedule_solution):
+    tab_time_cars = []
+    for car in list_cars:
+        total_time = 0
+        for ind_street_name in range(len(car)):
+            street_name = car[ind_street_name]
+            id_inters_end_street = streets[street_name][1]
+            if ind_street_name > 0:
+                time_travel_street = streets[street_name][2]
+                total_time += time_travel_street
+            if id_inters_end_street in dict_schedule_solution:
+                current_dict_inters = dict_schedule_solution[id_inters_end_street]
+                magic_number_wait = 0.05
+                total_time += magic_number_wait*len(current_dict_inters)
+
+        tab_time_cars.append(total_time)
+    return np.array(tab_time_cars)
 
 def build_dict_inters_car(list_cars, streets):
     full_dict_inters_car = {}
@@ -19,6 +45,8 @@ def build_dict_inters_car(list_cars, streets):
                 full_dict_inters_car[id_inters_end_street][street_name] = 1
 
     return full_dict_inters_car
+
+# def compute_schedule(dict_dict_street_time_arrived, schedule_inters):
 
 
 def build_dummy_schedule(full_dict_inters_car):
@@ -43,51 +71,39 @@ def build_dummy_schedule(full_dict_inters_car):
 
     return dict_schedule_solution
 
-
-def write_file_cheat(filename, intersection_dict):
-    """
-    # intersection_dict
-    {
-        3: [(rue-paris, 3), (rue-rome, 10)],
-        12: [(rue-paris, 3), (rue-rome2, 10)]
-    }
-    """
-    with open("output/{0}".format(filename), 'a') as the_file:
-        the_file.truncate(0)
-        the_file.write("{0} \n".format(len(intersection_dict))) # nbr intersection_dict
-        for key, value in intersection_dict.items():
-            the_file.write("{0} \n".format(key)) # intersection ID
-            the_file.write("{0} \n".format(len(value))) # nbr street for schedule in intersection
-
-            for value_car in sorted(value, key = lambda i: i[1]):
-                the_file.write("{0} {1}\n".format(value_car[0],value_car[1])) # for each street in the intersection : street_name time
-
-def processFile(name_file):
-    streets, cars, duration, nb_inters, nb_streets, nb_cars, bonus = read_file(os.path.join("input", name_file))
+tab_file = ["a.txt","b.txt","c.txt","d.txt","e.txt","f.txt"]
+for file in tab_file:
+    input_folder = "input"
+    name_file = file
+    streets, cars, duration, nb_inters, nb_streets, nb_cars, bonus = read_file(os.path.join(input_folder, name_file))
+    duration = int(duration)
     list_cars = cars
-
+    print("duration = ", duration)
     start_time = time.time()
     # print("time starting build_dict_inters_car = ", start_time)
-    full_dict_inters_car = build_dict_inters_car(list_cars, streets)
-    #print("duration build_dict_inters_car = ", time.time()-start_time)
+    tab_time_cars = compute_length_path_car(list_cars, streets)
+    indice_cars_usefull = np.where(tab_time_cars <= duration)[0]
+    list_cars_usefull = [list_cars[indice_car_usefull] for indice_car_usefull in indice_cars_usefull]
+
+    full_dict_inters_car = build_dict_inters_car(list_cars_usefull, streets)
+    print("duration build_dict_inters_car = ", time.time()-start_time)
     start_time = time.time()
     dict_schedule_solution = build_dummy_schedule(full_dict_inters_car)
-    #print("duration build_dummy_schedule = ", time.time()-start_time)
+    print("duration build_dummy_schedule = ", time.time()-start_time)
 
-    #print("dict_schedule_solution = ", dict_schedule_solution)
+    nb_iter = 0
+    for iter in range(nb_iter):
+        tab_time_cars_with_solution = compute_length_path_car_with_solution(list_cars_usefull, streets, dict_schedule_solution)
 
-    write_file_cheat(name_file, dict_schedule_solution)
+        indice_cars_usefull = np.where(tab_time_cars_with_solution <= duration)[0]
+        list_cars_usefull = [list_cars_usefull[indice_car_usefull] for indice_car_usefull in indice_cars_usefull]
 
-name_files = [
-"a.txt",
-"b.txt",
-"c.txt",
-"d.txt",
-"e.txt",
-"f.txt"
-]
+        full_dict_inters_car = build_dict_inters_car(list_cars_usefull, streets)
+        print("duration build_dict_inters_car = ", time.time() - start_time)
+        start_time = time.time()
+        dict_schedule_solution = build_dummy_schedule(full_dict_inters_car)
+        print("duration build_dummy_schedule = ", time.time() - start_time)
 
-for name in name_files:
-  print(name)
-  processFile(name)
-  print("\n\n")
+    # print("dict_schedule_solution = ", dict_schedule_solution)
+
+    write_file_normer(name_file, dict_schedule_solution,3)
