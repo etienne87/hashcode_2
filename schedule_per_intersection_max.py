6acd8f540640ce28e5edfa34aea940d6786709a7
Maximilian Jaritz <maxjaritz@gmail.com>
2021-02-25 21:44:34 +0100

    wait per intersection

--- schedule_per_intersection_max.py
+++ schedule_per_intersection_max.py
@@ -1,5 +1,6 @@
 import math
 
+
 def compute_wait_time(street_to_arrival_times_map: dict, inters_schedule: list, max_sim_time: int) -> int:
     """
     Computes the total waiting time of all cars at this intersection.
@@ -7,14 +8,7 @@ def compute_wait_time(street_to_arrival_times_map: dict, inters_schedule: list, 
     :param street_to_arrival_times_map: A dictionary that maps from street name (str) to list of arrival times (int)
     :return: total wait time (int)
     """
-    max_arrival_time = -1
-
-    for street_name, arrival_times in street_to_arrival_times_map.items():
-        max_arrival_time = max(arrival_times[-1], max_arrival_time)
-
-
     #  schedule: ['rue-paris', 'rue-paris', 'rue-barcelone']
-    t = 0
     cycle = [duration * [street] for street, duration in inters_schedule]
     factor = math.ceil(max_sim_time / len(cycle))
     schedule = factor * cycle
@@ -40,6 +34,7 @@ def compute_wait_time(street_to_arrival_times_map: dict, inters_schedule: list, 
         total_wait_time += sum(street_to_num_waiting_cars_map.values())
 
     return total_wait_time
+
 
 if __name__ == '__main__':
     street_to_arrival_times_map = {
