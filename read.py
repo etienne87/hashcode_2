ae72fc419b76e35b04f201ab1834056386f1ff2d
marintoro <marin.toromanoff@gmail.com>
2021-02-25 20:04:26 +0100

    Fix format write

--- read.py
+++ read.py
@@ -24,21 +24,30 @@ def read_file(filename):
 
 
 def write_file(filename, intersection_dict):
+    """
+    # intersection_dict
+    {
+        3: [(rue-paris, 3), (rue-rome, 10)],
+        12: [(rue-paris, 3), (rue-rome2, 10)]
+    }
+    """
+
+
     with open("output/{0}".format(filename), 'a') as the_file:
         the_file.truncate(0)
         the_file.write("{0} \n".format(len(intersection_dict))) # nbr intersection_dict
         for key, value in intersection_dict.items():
             the_file.write("{0} \n".format(key)) # intersection ID
             the_file.write("{0} \n".format(len(value))) # nbr street for schedule in intersection
-            for key_car, value_car in value.items():
-                the_file.write("{0} {1}\n".format(key_car,value_car)) # for each street in the intersection : street_name time
+            for value_car in value:
+                the_file.write("{0} {1}\n".format(value_car[0],value_car[1])) # for each street in the intersection : street_name time
 
 
 if __name__ == '__main__':
     streets, cars, duration, nb_inters, nb_streets, nb_cars, bonus = read_file('input/a.txt')
     print(streets)
     print(cars)
-    write_file('a.txt',{1: {'street_name':1},2: {'stree_name_2':2}})
+    write_file('a.txt',{1: [('rue-paris', 3), ('rue-rome', 10)] ,2: [('rue-paris', 3), ('rue-rome2', 10)]})
 
 
 
