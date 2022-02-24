8690b5b31e16331241ce0a9886c4ef1c1e65836a
marintoro <marin.toromanoff@gmail.com>
2022-02-24 20:03:19 +0100

    fonctun to choose project

--- marin.py
+++ marin.py
@@ -2,10 +2,10 @@ from read import read_file
 
 tab_file_to_read = ["input/a_an_example.in.txt",
                 "input/b_better_start_small.in.txt",
-"input/c_collaboration.in.txt",
-"input/d_dense_schedule.in.txt",
-"input/e_exceptional_skills.in.txt",
-"input/f_find_great_mentors.in.txt" ]
+                "input/c_collaboration.in.txt",
+                "input/d_dense_schedule.in.txt",
+                "input/e_exceptional_skills.in.txt",
+                "input/f_find_great_mentors.in.txt" ]
 
 indice_file = 0
 
@@ -21,10 +21,33 @@ print("nb_projects = ", nb_projects)
 projects = readed_file.projects
 print("projects = ", projects)
 
-def score_project(project, time_start):
+list_projects = projects.items()
+print("list_projects = ", list_projects)
+
+time_start = 0
+nb_contributors_available = nb_contributors
+def score_project(project, time_start, nb_contributors_available):
+    print("project in score_project = ", project)
+    print("skill_required = ", project.skill_required)
     days = project.d
     score = project.s
     best_before = project.b
     nb_roles = project.r
 
-    if time_start + days <
+    if nb_roles > nb_contributors_available:
+        return 0
+    if (time_start + days) <= best_before:
+        return score/(nb_roles*days)
+    else:
+        return (score - (best_before - time_start - days))/(nb_roles*days)
+
+def naive_assign_contrib_to_project(project, map_map_skill, dict_contributors):
+    dict_skill_needed =  project.skill_required
+    for skill in dict_skill_needed:
+        print("skill = ", skill)
+
+sorted_project = sorted(list_projects, key=lambda x: score_project(x[1], time_start, nb_contributors_available), reverse=True)
+print("sorted_project = ", sorted_project)
+
+naive_assign_contrib_to_project(sorted_project[0], None, None)
+
