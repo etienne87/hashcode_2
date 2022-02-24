1729ec370df8531cd0446b053104cc281e74c7f0
etienne <et.perot@gmail.com>
2022-02-24 20:27:39 +0100

    test

--- etienne.py
+++ etienne.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
 
 def make_map_of_skills(data):
-    skill_map = defaultdict(dict)
+    skill_map = defaultdict(lambda:defaultdict(set))
     for contrib_name,skill_set in data.contributors.items(): 
         for skill_name, value in skill_set.items():
-            if value not in skill_map[skill_name]:
-                skill_map[skill_name][value] = set()
+            #if value not in skill_map[skill_name]:
+            #    skill_map[skill_name][value] = set()
             skill_map[skill_name][value].add(contrib_name)
     return skill_map
 
@@ -15,10 +15,11 @@ def remove_contrib_from_skill_map(skill_map, contrib, skillset):
         skill_map[skill_name][skill_value].remove(contrib)
 
 
+
 def update_contributors(skills_required, selected, contributors, skill_map):
     for contrib_name in selected:
-        for skill_name in skills_required.keys():
-            old_value = contributors[contrib_name][skill_name]
-            skill_map[skill_name][old_value].remove(contrib_name)
-            skill_map[skill_name][old_value+1].add(contrib_name)
+        for skill in skills_required:
+            skill_name, skill_value = list(skill.items())[0]
             contributors[contrib_name][skill_name] += 1
+            value = contributors[contrib_name][skill_name]
+            skill_map[skill_name][value].add(contrib_name)
